496fbccbb83ee4a77e40b67df77cc35fd7268520
aandres44 <chuy27arts@gmail.com>
2020-06-03 04:42:54 +0000

    Practica 13

--- practica12/replace.c
+++ practica12/replace.c
@@ -1,44 +1,52 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <string.h>
 
+void writeToFile(int tmp, char *word, char *searchWord, char *replaceWord) {
+    if (strncmp(word, searchWord, strlen(searchWord)) == 0) {
+        write(tmp, replaceWord, strlen(replaceWord));
+    } else {
+        write(tmp, word, strlen(word));
+    }
+}
 int main(int argn, char **argv) {
     char *fileName = argv[3];
     char *searchWord = argv[1];
     char *replaceWord = argv[2];
+    char *tmpFileName = "/tmp/replace.txt";
     char word[100];
     int i = 0;
     char character;
     char espacio = 0;
     int fd = open(fileName, O_RDONLY);
-    if(fd < 0) {
+    int tmp = open(tmpFileName, O_WRONLY | O_CREAT, 0600);
+    if (fd < 0) {
         printf("No se puede abrir \n");
         exit(1);
     }
-    while(read(fd, &character, 1)) {
-        if((character == ' ' || character == '\n' || character == EOF) && espacio == 0) {
+    while (read(fd, &character, 1)) {
+        if ((character == ' ' || character == '\n') && espacio == 0) {
             espacio = 1;
+            word[i++] = character;
             word[i] = '\0';
             i = 0;
-            if(strcmp(word, searchWord) == 0) {
-                printf("%s \n", replaceWord);
-            } else {
-                printf("%s \n", word);
-            }
-            printf("%s \n", word);
-        } else if (character != ' ' && character != '\n' && character != EOF) {
+            writeToFile(tmp, word, searchWord, replaceWord);
+        } else if (character != ' ' && character != '\n') {
             espacio = 0;
             word[i++] = character;
         }
-        
     }
     word[i] = '\0';
-    printf("%s \n", word);
+    writeToFile(tmp, word, searchWord, replaceWord);
     close(fd);
+    close(tmp);
+    //renombrar archivo original
+    unlink();//borramos original
+    link(tmp, siper);//linkeamos al temporal
+    unlink(tmp);//borramos temporal
     exit(0);
 }
